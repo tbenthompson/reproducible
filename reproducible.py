7fff152e3c8640b65e96f4795e88371d29757367
Ben Thompson <t.ben.thompson@gmail.com>
2014-05-24 02:42:54 -0400

    Simple data storage and reloading.

---
+++ reproducible.py
@@ -0,0 +1,88 @@
+import os
+import time
+import errno
+import pickle
+import inspect
+import tarfile
+import tempfile
+import StringIO
+
+# Configurations
+cfg = dict()
+cfg['data_dir'] = 'data'
+cfg['fnc_filename'] = 'fnctext.txt'
+cfg['data_filename'] = 'saveddata.pkl'
+
+def setup_files(model_name):
+    directory = cfg['data_dir'] + '/' + model_name
+    try:
+        os.makedirs(directory)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+    return directory
+
+def write_string_to_tar(filename, s, tar_f):
+    string = StringIO.StringIO()
+    string.write(s)
+    string.seek(0)
+    info = tarfile.TarInfo(name = filename)
+    info.size = len(string.buf)
+    info.mtime = time.time()
+    tar_f.addfile(tarinfo = info, fileobj = string)
+
+def compare_steps(s, tar_f):
+    saved = tar_f.extractfile(cfg['fnc_filename']).read()
+    new = inspect.getsourcelines(s)
+    # StringIO does some funky escaping when we write to it
+    # so in order to compare we must write to it again
+    string = StringIO.StringIO()
+    string.write(new)
+    return string.getvalue() == saved
+
+
+"""
+A class for organizing repeatable, reproducible model runs.
+"""
+class Reproducible(object):
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.steps = []
+        self.directory = setup_files(model_name)
+        self.data = dict()
+
+    def add_step(self, step):
+        self.steps.append(step)
+
+    def run(self):
+        for s in self.steps:
+            if self.pre_step(s):
+                continue
+            s(self.data)
+            self.post_step(s)
+
+    def _get_tar_filename(self, step):
+        step_name = step.__name__
+        return self.directory + '/' + step_name + '.tar'
+
+    def pre_step(self, step):
+        file_name = self._get_tar_filename(step)
+        if not os.path.exists(file_name):
+            return False
+        with tarfile.open(file_name, 'r') as tar_f:
+            same_fnc = compare_steps(step, tar_f)
+            if not same_fnc:
+                return False
+            data_file = tar_f.extractfile(cfg['data_filename']).read()
+            self.data = pickle.loads(data_file)
+            return True
+
+    def post_step(self, step):
+        file_name = self._get_tar_filename(step)
+        function_text = inspect.getsourcelines(step)
+        data_dump = pickle.dumps(self.data)
+        with tarfile.open(file_name, 'w') as f:
+            write_string_to_tar(cfg['fnc_filename'], function_text, f)
+            write_string_to_tar(cfg['data_filename'], data_dump, f)
+
+
---
+++ test.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+import tarfile
+from reproducible import Reproducible, setup_files, compare_steps
+
+def test_initial():
+    model = Reproducible("test1")
+    model.run()
+
+def test_one_step():
+    def do_nothing(model):
+        pass
+    model = Reproducible("test2")
+    model.add_step(do_nothing)
+    model.run()
+
+def test_real_step():
+    def set_data(data):
+        data['data'] = "hello"
+    model = Reproducible("test3")
+    model.add_step(set_data)
+    model.run()
+    assert(model.data['data'] == "hello")
+
+def test_compare_steps():
+    def set_data(data):
+        data['data'] = "hello"
+    model = Reproducible("test3")
+    model.add_step(set_data)
+    model.run()
+    with tarfile.open('./data/test3/set_data.tar', 'r') as f:
+        assert(compare_steps(set_data, f))
+
+def test_setup_files():
+    if os.path.exists('./data/test3'):
+        shutil.rmtree('./data/test3')
+    setup_files('test3')
+    assert(os.path.exists('./data/test3'))
+
+def test_data_directory():
+    if os.path.exists('./data/test3'):
+        shutil.rmtree('./data/test3')
+    model = Reproducible("test3")
+    assert(os.path.exists('./data/test3'))
+
+def test_saved_step():
+    def set_data(data):
+        if set_data.idx > 0:
+            data['data'] = "uhoh!"
+            return
+        data['data'] = "hello"
+        set_data.idx += 1
+    set_data.idx = 0
+
+    model = Reproducible("test3")
+    model.add_step(set_data)
+    model.run()
+    assert(model.data['data'] == "hello")
+
+    model = Reproducible("test3")
+    model.add_step(set_data)
+    assert(not 'data' in model.data)
+    model.run()
+    assert(model.data['data'] == "hello")
